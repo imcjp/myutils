a7293f8f4a8e81dc0f6c8434e5cfe3835e9de2a5
imcjp <1107066941@qq.com>
2024-01-20 23:05:34 +0800

    Update setup.sh

--- frpcDeploy/setup.sh
+++ frpcDeploy/setup.sh
@@ -21,4 +21,20 @@ sudo ln -s "${current_dir}/${dirName}/systemd/frpc@.service" /etc/systemd/system
 # 步骤4: 赋予frpc文件可执行权限
 chmod 555 "${current_dir}/${dirName}/frpc"
 
+# 提示用户关于配置文件的信息
+echo "在 ${current_dir}/${dirName} 下有配置示例文件 frpc.ini.example。" > "${current_dir}/${dirName}/readme.txt"
+echo "您可以参考此示例文件编写您的配置文件，例如 XXX.ini。" >> "${current_dir}/${dirName}/readme.txt"
+echo "然后，通过服务 frpc@XXX.service 运行 frpc，其中 XXX 指代您的配置文件名称。" >> "${current_dir}/${dirName}/readme.txt"
+echo "请确保您的配置文件后缀为 *.ini。" >> "${current_dir}/${dirName}/readme.txt"
+
+# 提示用户如何使用 frpc@.service
+echo "使用方法：" >> "${current_dir}/${dirName}/readme.txt"
+echo "1. 编写配置文件并放置在 ${current_dir}/${dirName} 目录下，例如 ${current_dir}/${dirName}/XXX.ini。" >> "${current_dir}/${dirName}/readme.txt"
+echo "2. 启动服务：sudo systemctl start frpc@XXX" >> "${current_dir}/${dirName}/readme.txt"
+echo "3. 开机自启服务：sudo systemctl enable frpc@XXX" >> "${current_dir}/${dirName}/readme.txt"
+echo "4. 查看服务状态：sudo systemctl status frpc@XXX" >> "${current_dir}/${dirName}/readme.txt"
+echo "5. 停止服务：sudo systemctl stop frpc@XXX" >> "${current_dir}/${dirName}/readme.txt"
+echo "6. 重启服务：sudo systemctl restart frpc@XXX" >> "${current_dir}/${dirName}/readme.txt"
+echo "请替换 XXX 为您的配置文件名称，不包含.ini后缀。" >> "${current_dir}/${dirName}/readme.txt"
+
 echo "脚本执行完成。"
