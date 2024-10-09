221177d589237c8d333ef380df8fef7133e475cd
imcjp <1107066941@qq.com>
2024-10-09 17:34:42 +0800

    update trojan

--- trojan/setup.sh
+++ trojan/setup.sh
@@ -3,12 +3,24 @@
 dirName="trojan"
 version="0.10.6"
 tar_file="trojan.tar.gz"
+
 # 步骤1: 获得home目录的路径
-cd "${dirName}" # 确保在原始目录下
 current_dir=$(pwd)
 echo "当前文件夹路径: $current_dir"
 
-# 步骤2: 使用wget下载trojan.zip并解压到一个随机命名的目录，确保在原始目录下进行
+# 使用wget下载scripts.zip并解压到一个随机命名的目录，确保在原始目录下进行
+cd "${current_dir}" # 确保在原始目录下
+dirName="trojan"
+tar_file="trojan.tar.gz"
+
+wget "https://raw.githubusercontent.com/imcjp/myutils/main/trojan/${tar_file}"
+tar -zxvf "${tar_file}" -C "${current_dir}" && rm "${tar_file}" # 解压并删除tar.gz文件
+echo "下载并解压了${tar_file}到 ${current_dir}/${dirName} 并删除了tar.gz文件"
+
+cd "${trojan}" # 确保在原始目录下
+current_dir=$(pwd)
+
+# 步骤2: 使用wget下载trojan安装包
 wget "https://github.com/p4gefau1t/trojan-go/releases/download/v${version}/${tar_file}"
 sudo apt install unzip
 unzip trojan-go-linux-amd64.zip
